be0d4b65bdd7ac1438780a7bbd6017a78d690a7d
StraytDZ <chihabmedjdoub@gmail.com>
2026-04-10 04:52:33 +0100

    Premier commit

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "reception.h"
+
+void menu(){
+    printf("\t+++++URGENCE+++++\n");
+    printf("\t=+=+=+=MENU=+=+=+=\n");
+    printf("=======================================\n\n");
+    printf("\t 1 - Medecin\n");
+    printf("\t 2 - Reception\n");
+    printf("\t 3 - Quitter\n\n");
+    printf("=======================================\n");
+    printf("                          Choix : ");
+}
+
+int main() {
+    int choix,choixReception,choixMedecin,Age;
+    char Nom[30],Prenom[30],ID[20],Sexe[4];
+    Patient *ListePatient = NULL;
+    ListeTicket ticket = {NULL, 0};
+
+    do {
+        menu();
+        scanf("%d",&choix);
+        switch(choix) {
+            case 1 : 
+            break;
+            
+            case 2 : 
+                    do{
+                        menuReception();
+                        scanf("%d",&choixReception);
+                        switch(choixReception) {
+                            case 1: 
+                                printf("Veuiller fournir les informations suivante du patient :");
+                                printf("\t\nNom : ");
+                                scanf(" %[^\n]",Nom);
+                                printf("\t\nPrenom : ");
+                                scanf(" %[^\n]",Prenom);
+                                printf("\t\nID : ");
+                                scanf(" %[^\n]",ID);
+                                printf("\t\nAge : ");
+                                scanf("%d",&Age);
+                                printf("\t\nSexe(H/F): ");
+                                scanf("%s",Sexe);
+                                ListePatient = AddPatient(ListePatient,&ticket,Nom,Prenom,ID,Age,Sexe);
+                            break;
+                            case 2 : 
+                                DisplayQueue(ListePatient);
+                            break;
+                        }
+                    }while(choixReception != 3);
+            break;
+        }  
+    }while(choix != 3);
+    return 0;
+}
---
+++ reception.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "reception.h"
+
+void menuReception() {
+    printf("\n=======================================\n");
+    printf("\t=+=+=+=MENU=+=+=+=\n");
+    printf("\t*+*+*RECEPTION*+*+*\n");
+    printf("=======================================\n\n");
+    printf("\t 1 - Enregister un patient\n");
+    printf("\t 2 - Afficher la liste d'attente\n");
+    printf("\t 3 - Quitter\n\n");
+    printf("=======================================\n");
+    printf("                          Choix : ");
+}
+
+Ticket *CreateTicket(Patient *patient, int num) {
+    Ticket *T = (Ticket*)malloc(sizeof(Ticket));
+        if(T == NULL) {
+            printf("Erreur : Impossible de cree un ticket.");
+            exit(1);
+        }
+    T->client = patient;
+    T->numero = num;
+    T->suivant = NULL;
+    return T;
+}
+Ticket *AddTicket(ListeTicket *ListeT, Patient *patient) {
+    ListeT->compteur++;
+    Ticket *nouveauTicket = CreateTicket(patient,ListeT->compteur);
+    if(ListeT->tete == NULL) { 
+        ListeT->tete = nouveauTicket;  
+        return nouveauTicket;
+         }
+    Ticket *courant = ListeT->tete;
+    while(courant->suivant != NULL) {
+        courant = courant->suivant;
+    }
+    courant->suivant = nouveauTicket;
+
+    return nouveauTicket;
+}
+Patient *AddPatient(Patient *ListeP,ListeTicket *ListeT, char Nom[],char Prenom[],char ID[],int Age, char Sexe[]) {
+    Patient *P = (Patient*)malloc(sizeof(Patient));
+        if(P == NULL){
+            printf("Erreur : Impossible d'enregister le patient.");
+            exit(1);
+        }
+    strcpy(P->nom, Nom);
+    strcpy(P->prenom, Prenom);
+    strcpy(P->id, ID);
+    strcpy(P->sexe, Sexe);
+    P->age = Age;
+    P->suivant = NULL;
+    P->numticket = AddTicket(ListeT,P);
+    if(ListeP == NULL) return P;
+    Patient *courant = ListeP;
+    while(courant->suivant != NULL)
+        courant = courant->suivant;
+    courant->suivant = P;
+
+    return ListeP;
+}
+void DisplayQueue(Patient *ListeP) {
+    Patient *courant = ListeP;
+    int i = 1;
+    while(courant != NULL) {
+        printf("Patient %d \n",i);
+        printf("\tNom : %s\n",courant->nom);
+        printf("\tPrenom : %s\n",courant->prenom);
+        printf("\tID : %s\n",courant->id);
+        printf("\tAge : %d\n",courant->age);
+        printf("\tSexe(H/F): %s\n",courant->sexe);
+        courant = courant->suivant;
+        i++;
+    }
+}
