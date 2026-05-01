2e6019d0cf1492fe41ed47b551355a7d1318049b
bellilrayane268-a11y <bellilrayane268@gmail.com>
2026-05-01 22:23:05 +0200

    add file.h

---
+++ file.h
@@ -0,0 +1,18 @@
+#ifndef FICHIER_H
+#define FICHIER_H
+
+#include "structure.h"
+
+/* wayi pour Sauvegarder */
+void         sauvegarderPatients(ListePatient *liste);
+void         sauvegarderObservations(Observation *lits);
+void         sauvegarderHistorique(Patient *p);
+
+/* pour  Chargement */
+void         chargerPatients(ListePatient *liste, ListeTicket *listeT);
+Observation *chargerObservations(ListePatient *liste, int *numeroPrecedent);
+
+/*pour  Historique */
+void         afficherHistorique();
+
+#endif
